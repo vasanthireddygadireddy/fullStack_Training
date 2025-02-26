07959c9d9644e121e214910f804c0c7dbb9fc461
vasanthi-reddy <vasanthireddy0605@gmail.com>
2025-02-25 18:23:25 -0600

    leetcode

---
+++ leetcode.js
@@ -0,0 +1,44 @@
+//Create Hello World Function
+function createHelloWorld() {
+  return function() {
+    return "Hello World";
+  };
+}
+const helloFn = createHelloWorld();
+console.log(helloFn()); 
+console.log(helloFn()); 
+
+//Counter
+function createCounter(n) {
+  return function() {
+    return n++;
+  };
+}
+const counter = createCounter(10);
+console.log(counter()); 
+console.log(counter()); 
+console.log(counter()); 
+
+//To Be Or not to be
+const expect = (val) => ({
+  toBe: (expected) => (val === expected ? true : (() => { throw new Error("Not Equal"); })()),
+  notToBe: (expected) => (val !== expected ? true : (() => { throw new Error("Equal"); })())
+});
+console.log(expect(5).toBe(5)); 
+console.log(expect(10).notToBe(5)); 
+
+//Counter II
+const createCounter = (init) => {
+  let current = init;
+  return {
+    increment: () => ++current,
+    decrement: () => --current,
+    reset: () => (current = init)
+  };
+};
+const counter = createCounter(10);
+console.log(counter.increment()); 
+console.log(counter.decrement()); 
+console.log(counter.reset());  
+
+//
